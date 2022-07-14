39e535ec7211d16a539a825bc539cfb3af48cffb
Koichi.Shimomura <unieye51@gmail.com>
2022-07-14 14:31:10 +0900

    8-46. Slots

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CardList from './components/CardList.vue';
+</script>
+
+<template>
+  <CardList></CardList>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
